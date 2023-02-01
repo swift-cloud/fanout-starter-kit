be8d73566c802ee3d63a835ff667cd3f7987fdac
Andrew Barba <barba@hey.com>
2023-02-01 12:26:58 -0500

    Fix backend name

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,7 +10,7 @@ router.get("/stream") { req, res in
     guard req.isFanoutRequest() else {
         return try await res.status(400).send("Invalid websocket request")
     }
-    try req.upgradeWebsocket(backend: "self", behavior: .fanout)
+    try req.upgradeWebsocket(backend: "localhost", behavior: .fanout)
 }
 
 router.post("/stream") { req, res in
