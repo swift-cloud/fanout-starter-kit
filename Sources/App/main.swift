ecb102bcf55b62c206c45d240c1355f23f593b61
Andrew Barba <barba@hey.com>
2023-02-01 14:54:58 -0500

    parse fanout message

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,32 +2,25 @@ import Compute
 
 let router = Router()
 
-router.use { req, _ in
-    console.log(req.method, req.url.path)
-}
-
 router.get("/stream") { req, res in
     guard req.isUpgradeWebsocketRequest() else {
-        console.log("Invalid websocket request")
         return try await res.status(400).send("Invalid websocket request")
     }
-    console.log("upgrading websocket...")
     try req.upgradeWebsocket(backend: "localhost", behavior: .fanout)
 }
 
 router.post("/stream") { req, res in
     guard req.isFanoutRequest() else {
-        console.log("Invalid fanout request")
         return try await res.status(400).send("Invalid fanout request")
     }
 
-    let body = try await req.body.text()
+    let message = try await req.fanoutMessage()
 
-    console.log("body:", body)
+    console.log("message:", message.event.rawValue, message.content)
 
     console.log("grip-sig:", req.headers[.gripSig] ?? "null")
 
-    if body.starts(with: "OPEN") {
+    if message.event == .open {
         try await res.status(200).send(.open, .subscribe(to: "test"))
     } else {
         try await res.status(200).send(.ack)
